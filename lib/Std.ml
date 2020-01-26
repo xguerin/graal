9d82d3709d4c620294219178e3ef1df4522c3731
Xavier R. Guérin <github@applepine.org>
2020-01-26 17:21:12 -0500

    Add subgraph example, add Std.duplicate operator

--- lib/Std.ml
+++ lib/Std.ml
@@ -1,17 +1,8 @@
 open Lwt.Infix
 
-class apply ~fn reader writer = object
-  inherit Types.operator
-
-  method process =
-    let rec process_r () =
-      reader#read
-      >>= fn
-      >>= writer#write
-      >>= process_r
-    in
-    process_r ()
-end
+(*
+ * Beacon.
+ *)
 
 class beacon ?(delay=1.0) ~zero ~next reader writer = object
   inherit Types.operator
@@ -28,6 +19,24 @@ class beacon ?(delay=1.0) ~zero ~next reader writer = object
     process_r ()
 end
 
+(*
+ * Functor and filter.
+ *)
+
+class apply ~fn reader writer = object
+  inherit Types.operator
+
+  method process =
+    let rec process_r () =
+      reader#read
+      >>= fn
+      >>= writer#write
+      >>= process_r
+    in
+    process_r ()
+end
+
+
 class filter ~fn reader writer = object
   inherit Types.operator
 
@@ -35,6 +44,51 @@ class filter ~fn reader writer = object
     let rec process_r () =
       reader#read
       >>= fun v -> if (fn v) then writer#write v else Lwt.return ()
+      >>= process_r
+    in
+    process_r ()
+end
+
+(*
+ * Subgraph input/output.
+ *)
+
+class input ~reader _ writer = object
+  inherit Types.operator
+
+  method process =
+    let rec process_r () =
+      reader#read
+      >>= writer#write
+      >>= process_r
+    in
+    process_r()
+end
+
+class output ~writer reader _ = object
+  inherit Types.operator
+
+  method process =
+    let rec process_r () =
+      reader#read
+      >>= writer#write
+      >>= process_r
+    in
+    process_r()
+end
+
+(*
+ * Merge/split.
+ *)
+
+class duplicate reader (w0, w1) = object
+  inherit Types.operator
+
+  method process =
+    let rec process_r () =
+      reader#read
+      >>= fun v -> w0#write v
+      >>= fun () -> w1#write v
       >>= process_r
     in
     process_r ()
@@ -53,18 +107,6 @@ class merge (r0, r1) writer = object
     process_r ()
 end
 
-class sink reader writer = object
-  inherit Types.operator
-
-  method process =
-    let rec process_r () =
-      reader#read
-      >>= fun _ -> writer#write ()
-      >>= process_r
-    in
-    process_r ()
-end
-
 class split reader (w0, w1) = object
   inherit Types.operator
 
@@ -73,6 +115,22 @@ class split reader (w0, w1) = object
       reader#read
       >>= fun (v0, v1) -> w0#write v0
       >>= fun () -> w1#write v1
+      >>= process_r
+    in
+    process_r ()
+end
+
+(*
+ * Generic sink.
+ *)
+
+class sink reader writer = object
+  inherit Types.operator
+
+  method process =
+    let rec process_r () =
+      reader#read
+      >>= fun _ -> writer#write ()
       >>= process_r
     in
     process_r ()
